85b2b9a2369c89f38cba6ebabfdd11a3d8335e6b
edson.villacorta <edson26web@gmail.com>
2026-02-12 15:24:37 -0400

    Agregar archivos de proyecto.

---
+++ MAUI Sample Proyects/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI_Sample_Proyects
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI Sample Proyects/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUI_Sample_Proyects
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
