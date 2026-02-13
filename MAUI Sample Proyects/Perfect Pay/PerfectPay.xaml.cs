8af93156d84b758223f9642f966e7969da9433ef
edson.villacorta <edson26web@gmail.com>
2026-02-13 10:43:54 -0400

    Created design

---
+++ MAUI Sample Proyects/Perfect Pay/PerfectPay.xaml.cs
@@ -0,0 +1,42 @@
+namespace MAUI_Sample_Proyects.Perfect_Pay;
+
+public partial class PerfectPay : ContentPage
+{
+	decimal bill;
+	int tip;
+	int noPersons = 1;
+
+
+
+	public PerfectPay()
+	{
+		InitializeComponent();
+	}
+
+    private void txtBill_Completed(object sender, EventArgs e)
+    {
+		bill = decimal.Parse(txtBill.Text);
+		CalculateTotal();
+    }
+
+    private void CalculateTotal()
+    {
+
+    }
+
+    private void sldTip_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        tip = (int)sldTip.Value;
+        lblTip.Text = $"Tip: {tip}%";
+        CalculateTotal();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        if (sender is Button) {
+            var btn = (Button)sender;
+            var percentage = int.Parse(btn.Text.Replace("%", ""));
+            sldTip.Value = percentage;
+        }
+    }
+}
